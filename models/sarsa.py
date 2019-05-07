fe2f4c5dbb42c2be1ce922935fce86186616f270
Zhiliang Zhou <zhouzhiliang@gmail.com>
2019-05-07 17:04:04 +0200

    * add setup parameters

--- models/sarsa.py
+++ models/sarsa.py
@@ -5,18 +5,28 @@ import sys
 
 
 class SARSA():
-    def __init__(self):
-        self.gamma = 1.0
-        self.alpha = 0.02
-        self.epsilon = 1.0
-        self.epsilon_start = 1.0
-        self.epsilon_decay=0.9999
-        self.epsilon_min = 0.05
-        self.nA = 4
-        self.action_space = [0, 1, 2, 3]  # up,right,down,left
+    def __init__(self,
+                 nA,
+                 gamma=0.9,
+                 alpha=0.2,
+                 epsilon_start=1.0,
+                 epsilon_decay=0.9999,
+                 epsilon_min=0.001,
+                 epsilon_fn=None):
+        self.nA = nA
+        self.action_space = list(range(self.nA))
+
+        self.gamma = gamma
+
+        self.alpha = alpha
+
+        self.epsilon = epsilon_start
+        self.epsilon_start = epsilon_start
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.update_epsilon = epsilon_fn if epsilon_fn is not None else self.decay_epislon
+
         self.Q = defaultdict(lambda: np.zeros(self.nA))
-        self.avg_rewards = None
-        self.max_steps_episode = 300
 
     def update(self, sarsa_sample):
         # update with Temporal-Difference instead of complete episode
@@ -24,6 +34,7 @@ class SARSA():
         s, a, r, s_next, a_next = sarsa_sample[0:5]
         old_Q = self.Q[s][a]
         self.Q[s][a] = old_Q + self.alpha * (r + self.gamma * self.Q[s_next][a_next] - old_Q)
+        self.update_epsilon()
 
     def choose_action(self, state):
         prob_choose = np.ones(self.nA) / self.nA * self.epsilon
@@ -38,6 +49,12 @@ class SARSA():
     def get_Qtable(self):
         return self.Q
 
+    def reset_epislon(self):
+        self.epsilon = self.epsilon_start
+
+    def decay_epislon(self):
+        self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+
 
 class MaxSARSA(SARSA):
     def update(self, sarsa_sample):
@@ -46,6 +63,7 @@ class MaxSARSA(SARSA):
         # trick action id = action value, so we can use self.Q[s][a] for q(s,a)
         old_Q = self.Q[s][a]
         self.Q[s][a] = old_Q + self.alpha * (r + self.gamma * np.max(self.Q[s_next]) - old_Q)
+        self.update_epsilon()
 
 
 class QLearning(SARSA):
@@ -55,6 +73,7 @@ class QLearning(SARSA):
         # trick action id = action value, so we can use self.Q[s][a] for q(s,a)
         old_Q = self.Q[s][a]
         self.Q[s][a] = old_Q + self.alpha * (r + self.gamma * np.max(self.Q[s_next]) - old_Q)
+        self.update_epsilon()
 
 
 class ExpectedSARSA(SARSA):
@@ -69,24 +88,24 @@ class ExpectedSARSA(SARSA):
         prob[np.argmax(action_value)] = 1 - (self.nA - 1.0) / self.nA * self.epsilon
 
         self.Q[s][a] = old_Q + self.alpha * (r + self.gamma * np.dot(self.Q[s_next], prob) - old_Q)
+        self.update_epsilon()
 
 
-def interact(env, agent, max_steps_episode=300, num_episodes=5000):
+def interact(env, agent, max_steps_episode=300, num_episodes=5000, window=100):
     avg_rewards = deque(maxlen=num_episodes)  # measure performance
 
-    for i_episode in range(num_episodes):
-        print("\rEpisode {}/{}".format(i_episode, num_episodes), end="")
-        sys.stdout.flush()
+    window_rewards = deque(maxlen=window)  # when full, oldest item will automatically pop up
 
+    for i_episode in range(num_episodes):
         # reset env for each episode
         state = env.reset()
         action = None
         # update epsilon external
-        agent.epsilon = 1.0 / (i_episode + 1)
+        # agent.epsilon = 1.0 / (i_episode + 1)
 
         step_counter = 0
         sum_reward = 0
-        succ_scores = deque(maxlen=max_steps_episode)
+
         while True:
             if action is None:
                 action = agent.choose_action(state)
@@ -103,13 +122,16 @@ def interact(env, agent, max_steps_episode=300, num_episodes=5000):
             step_counter += 1
 
             if done is True:
-                succ_scores.append(np.mean(sum_reward))
+                window_rewards.append(sum_reward)
                 break
             elif step_counter > max_steps_episode:
                 break
 
-        if len(succ_scores) > 0:
-            avg_rewards.append(np.mean(succ_scores))
+        if len(window_rewards) > 0:
+            avg_rewards.append(np.mean(window_rewards))
+
+        print("\rEpisode {}/{} || Best average reward {}".format(i_episode, num_episodes, max(avg_rewards)), end="")
+        sys.stdout.flush()
 
     return agent, avg_rewards
 
@@ -119,11 +141,12 @@ if __name__ == "__main__":
     import time
     import matplotlib.pyplot as plt
 
-    agent_sarsa = SARSA()
-    agent_maxsarsa = MaxSARSA()
-    agent_expsarsa = ExpectedSARSA()
-
     env = gym.make('CliffWalking-v0')
+
+    agent_sarsa = SARSA(env.action_space.n)
+    agent_maxsarsa = MaxSARSA(env.action_space.n)
+    agent_expsarsa = ExpectedSARSA(env.action_space.n)
+
     start = time.time()
     agent_sarsa, avg_rewards_sarsa = interact(env, agent_sarsa, max_steps_episode=300, num_episodes=5000)
     print("\nSARSA total time = ", time.time()-start)
@@ -134,16 +157,8 @@ if __name__ == "__main__":
     agent_expsarsa, avg_rewards_exppsarsa = interact(env, agent_expsarsa, max_steps_episode=300, num_episodes=5000)
     print("\nExpected SARSA total time = ", time.time()-start)
 
-    def smooth(y, box_pts):
-        """
-        moving average smooth
-        """
-        box = np.ones(box_pts) / box_pts
-        y_smooth = np.convolve(y, box, mode='same')
-        return y_smooth
-
-    plt.plot(smooth(np.array(avg_rewards_sarsa), 50), c="r")
-    plt.plot(smooth(np.array(avg_rewards_maxsarsa), 50), c="g")
-    plt.plot(smooth(np.array(avg_rewards_exppsarsa), 50), c="b")
+    plt.plot(np.array(avg_rewards_sarsa), c="r")
+    plt.plot(np.array(avg_rewards_maxsarsa), c="g")
+    plt.plot(np.array(avg_rewards_exppsarsa), c="b")
 
     plt.show()
