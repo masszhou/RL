6bd93337c25d217e6f5f978595a42d843319474f
Zhiliang Zhou <zhouzhiliang@gmail.com>
2019-06-03 21:48:57 +0200

    + change memory format

--- models/DQN.py
+++ models/DQN.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from _collections import deque
+from collections import deque
 
 
 class DeepQNetwork():
@@ -48,6 +48,8 @@ class DeepQNetwork():
         self.memory_size = memory_size
         self.memory = deque(maxlen=self.memory_size)
 
+        tf.reset_default_graph()
+
         self.batch_size = 32
 
         t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='net_target')
@@ -66,6 +68,7 @@ class DeepQNetwork():
         # must be the last line of init
         # must after graph definition
         self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
 
     def build_network(self):
         self.s = tf.placeholder(tf.float32, [None, self.n_features], name="s")
@@ -118,12 +121,17 @@ class DeepQNetwork():
             self.train_op = optimizer.minimize(self.end_points["loss"])
 
     def store_transition(self, s, a, r, s_next):
-        self.memory.append([s, a, r, s_next])  # deque
+        # assert s,s_next -> np.array
+        # assert a -> scalar
+        # assert r -> scalar
+        self.memory.append(np.concatenate([s, [a], [r], s_next])) # deque
 
     def choose_action(self, state):
         prob_choose = np.ones(self.n_actions) / self.n_actions * self.epsilon
-        action_value = self.sess.run(self.end_points["Q_eval"], feed_dict={self.s: state})
-        prob_choose[np.argmax(action_value)] = 1 - (self.nA - 1.0)/self.nA * self.epsilon
+        s = np.expand_dims(state, axis=0)
+        action_value = self.sess.run(self.end_points["Q_eval"], feed_dict={self.s: s})
+        # epsilon greedy
+        prob_choose[np.argmax(action_value)] = 1 - (self.n_actions - 1.0)/self.n_actions * self.epsilon
         action = np.random.choice(self.action_space, p=prob_choose)
         return action
 
@@ -135,13 +143,16 @@ class DeepQNetwork():
 
         memory_size = len(self.memory)
         sample_index = np.random.choice(range(memory_size), size=self.batch_size)  # replace = False
-        batch_memory = [self.memory[i] for i in sample_index]
+        batch_memory = np.array([self.memory[i] for i in sample_index])
 
-        s, a, r, s_next = zip(*batch_memory)
-        feed_dict = {self.s: np.array(s, dtype=np.float),
-                     self.a: np.array(a, dtype=np.int32),
-                     self.r: np.array(r, dtype=np.float32),
-                     self.s_next: np.array(s_next, dtype=np.float)}
+        s = batch_memory[:, 0:self.n_features]
+        a = batch_memory[:, self.n_features:self.n_features+1]
+        r = batch_memory[:, self.n_features+1:self.n_features+2]
+        s_next = batch_memory[:, self.n_features+2:]
+        feed_dict = {self.s: s.astype(np.float32),
+                     self.a: a.astype(np.int32),
+                     self.r: r.astype(np.float32),
+                     self.s_next: s_next.astype(np.float32)}
 
         _, cost = self.sess.run([self.train_op, self.end_points["loss"]], feed_dict=feed_dict)
         self.cost_list.append(cost)
@@ -180,10 +191,12 @@ if __name__ == "__main__":
     import gym
 
     env = gym.make('CartPole-v0')  # 定义使用 gym 库中的那一个环境
+    # https://github.com/openai/gym/wiki/CartPole-v0
     env = env.unwrapped  # 不做这个会有很多限制
 
     print(env.action_space)  # 查看这个环境中可用的 action 有多少个
     print(env.observation_space)  # 查看这个环境中可用的 state 的 observation 有多少个
+    # [position of cart, velocity of cart, angle of pole, rotation rate of pole]
     print(env.observation_space.high)  # 查看 observation 最高取值
     print(env.observation_space.low)  # 查看 observation 最低取值
 
