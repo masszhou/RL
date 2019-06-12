30d06a21671e403c6aca5bd299f37e261a287320
Zhiliang Zhou <zhouzhiliang@gmail.com>
2019-06-12 14:40:02 +0200

    ** two bug fixed\n 1. default learning rate 0.001 was too small, recommended 0.01\n 2. rewrad +1 with done==True should be negative due to end game action, or use customized reward

--- models/DQN.py
+++ models/DQN.py
@@ -6,22 +6,27 @@ from collections import deque
 class DeepQNetwork:
     """
     recall Q-Learning, how to update Q table
-    old_Q = Q[s][a]
-    Q[s][a] = old_Q + alpha * (r + gamma * np.max(Q[s_next]) - old_Q)
+    Q[s][a] = Q[s][a] + alpha * (r + gamma * np.max(Q[s_next]) - Q[s][a])
 
     notation in DQN
-    Q_eval = net_eval(s), here is Q values with all actions
+    Q_eval = eval_net(s), here is Q values with all actions
     Q_eval_wrt_a = Q[s][a] = Q_eval[a]
 
-    Q_next = net_target(s_next) = Q[s_next]
+    Q_next = target_net(s_next) = Q[s_next]
     Q_target = r + gamma * np.max(Q_next)
 
-    Loss = L2_Norm(Q_target - Q_eval_wrt_a) -> update Net_eval
-    hard copy net_eval to net_target, e.g. every 300 iterations of Net_eval
+    due to contraction mapping of Bellman operator
+    min L2_Norm(Q_target - Q_eval_wrt_a) to find the fix point of
+    Q* = BQ*
+    so, define
+    Loss = L2_Norm(Q_target - Q_eval_wrt_a) -> update eval_net
+    hard copy eval_net to target_net, e.g. every 300 iterations of eval_net
 
     refs
-    1. Udacity, Deep Reinforcement Learning
-    2. MorvanZhou, DQN Tutorial
+    1. Udacity, Deep Reinforcement Learning Nanodegree
+    2. Udacity, ud600 Reinforcement Learning
+    2. DQN Tutorial by MorvanZhou
+    3. Cartpole - Introduction to Reinforcement Learning (DQN - Deep Q-Learning) by Greg Surma
     """
     def __init__(self,
                  n_actions,
@@ -29,119 +34,103 @@ class DeepQNetwork:
                  gamma=0.95,
                  learning_rate=0.01,
                  epsilon_start=1.0,
-                 epsilon_decay=0.9999,
-                 epsilon_min=0.01,
-                 epsilon_fn=None,
-                 memory_size=20000):
+                 epsilon_decay=0.999,
+                 epsilon_min=0.05,
+                 replace_target_iter=100,
+                 batch_size=32,
+                 memory_size=20000,
+                 output_graph=True):
 
         self.n_actions = n_actions
-        self.action_space = list(range(self.n_actions))
         self.n_features = n_features
+        self.lr = learning_rate
 
-        self.gamma = gamma  # score_log discount factor
+        self.gamma = gamma  # reward decay
 
-        self.epsilon = epsilon_start  # greedy epsilon
-        self.epsilon_start = epsilon_start
-        self.epsilon_decay = epsilon_decay
-        self.epsilon_min = epsilon_min
-        self.update_epsilon = epsilon_fn if epsilon_fn is not None else self.decay_epislon
-
-        self.learning_rate = learning_rate  # learning rate
+        self.replace_target_iter = replace_target_iter
 
         self.memory_size = memory_size
+        self.batch_size = batch_size
+
+        self.epsilon = epsilon_start
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+
+        # total learning step
+        self.learn_step_counter = 0
+
+        # initialize zero memory [s, a, r, s_]
         self.memory = deque(maxlen=self.memory_size)
 
-        tf.reset_default_graph()
+        # consist of [target_net, evaluate_net]
+        # ------------------ all inputs ------------------------
+        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input State
+        self.s_next = tf.placeholder(tf.float32, [None, self.n_features], name='s_')  # input Next State
+        self.r = tf.placeholder(tf.float32, [None, ], name='r')  # input Reward
+        self.a = tf.placeholder(tf.int32, [None, ], name='a')  # input Action
+        self.endpoints = {}
+        self.build_network()
 
-        self.batch_size = 50
+        t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_net')
+        e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='eval_net')
 
-        t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='net_target')
-        e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='net_eval')
-        # define update net_target op
         with tf.variable_scope('hard_replacement'):
             self.target_replace_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
 
-        # define network
-        self.s = tf.placeholder(tf.float32, [None, self.n_features], name="s")
-        self.a = tf.placeholder(tf.int32, [None, ], name="a")  # shape=(?,)
-        self.r = tf.placeholder(tf.float32, [None, ], name="r")  # shape=(?,)
-        self.s_next = tf.placeholder(tf.float32, [None, self.n_features], name="s_next")
-        self.end_points = {}
-        self.build_network()
-
-        self.learn_step_counter = 0
-        self.learn_upadte_steps = 300  # update net_target while net_eval updates every 300 times
-
-        self.cost_list = []
-
-        # must be the last line of init
-        # must after graph definition
         self.sess = tf.Session()
+
+        if output_graph:
+            # $ tensorboard --logdir=logs
+            tf.summary.FileWriter("logs/", self.sess.graph)
+
         self.sess.run(tf.global_variables_initializer())
+        self.cost_log = []
 
     def build_network(self):
-        w_initializer = tf.keras.initializers.he_normal()
+        w_initializer = tf.random_normal_initializer(0., 0.3)
         b_initializer = tf.constant_initializer(0.1)
 
-        with tf.variable_scope("net_eval") as scope:
-            net_eval = tf.keras.layers.Dense(24,
+        with tf.variable_scope("eval_net") as scope:
+            eval_net = tf.keras.layers.Dense(20,
                                              activation=tf.keras.activations.relu,
                                              kernel_initializer=w_initializer,
                                              bias_initializer=b_initializer,
                                              name="dense")(self.s)
-            net_eval = tf.keras.layers.Dense(24,
+            eval_net = tf.keras.layers.Dense(self.n_actions,
                                              activation=tf.keras.activations.relu,
                                              kernel_initializer=w_initializer,
                                              bias_initializer=b_initializer,
-                                             name="dense")(net_eval)
-            net_eval = tf.keras.layers.Dense(self.n_actions,
-                                             activation=tf.keras.activations.relu,
-                                             kernel_initializer=w_initializer,
-                                             bias_initializer=b_initializer,
-                                             name="Q_eval")(net_eval)
-            # Q_eval = net_eval(s) = old_Q
-            self.end_points["Q_eval"] = net_eval  # [n_batch, n_actions]
+                                             name="Q_eval")(eval_net)
+            self.endpoints["Q_eval"] = eval_net # [n_batch, n_actions]
             # note, Q_eval(shape=(?, 2)) has Q values with all actions
 
-            # here we slice elements (indices=(n_actions, a)) from Q_eval and build a list wrt batch
-            # suppose a has shape (n_batches,)
             a_indices = tf.stack([tf.range(tf.shape(self.a)[0], dtype=tf.int32), self.a], axis=1)
             # a_indices has shape (n_batch, 2) like [[0, action], [1, action], [2, action], ... ,[n_batches-1, action]]
+            self.endpoints["Q_eval_wrt_a"] = tf.gather_nd(self.endpoints["Q_eval"], indices=a_indices)  # shape=(None, )
 
-            # Q_eval_wrt_a = Q[s][a] = Q_eval[a]
-            self.end_points["Q_eval_wrt_a"] = tf.gather_nd(net_eval, indices=a_indices)  # shape=(None, )
-
-        with tf.variable_scope("net_target") as scope:
-            net_target = tf.keras.layers.Dense(24,
+        with tf.variable_scope("target_net") as scope:
+            target_net = tf.keras.layers.Dense(20,
                                                activation=tf.keras.activations.relu,
                                                kernel_initializer=w_initializer,
                                                bias_initializer=b_initializer,
                                                name="dense")(self.s_next)
-            net_target = tf.keras.layers.Dense(24,
+            target_net = tf.keras.layers.Dense(self.n_actions,
                                                activation=tf.keras.activations.relu,
                                                kernel_initializer=w_initializer,
                                                bias_initializer=b_initializer,
-                                               name="dense")(net_target)
-            net_target = tf.keras.layers.Dense(self.n_actions,
-                                               activation=tf.keras.activations.relu,
-                                               kernel_initializer=w_initializer,
-                                               bias_initializer=b_initializer,
-                                               name="Q_next")(net_target)
-            self.end_points["Q_next"] = net_target
+                                               name="Q_next")(target_net)
+            self.endpoints["Q_next"] = target_net
             # Q_target = r + gamma * np.max(Q_next)
-            net_target = self.r + self.gamma * tf.math.reduce_max(net_target, axis=1, name="Q_max")
-            # Important! not calculating gradient from this op
-            self.end_points["Q_target"] = tf.stop_gradient(net_target)
+            q_target = self.r + self.gamma * tf.math.reduce_max(self.endpoints["Q_next"], axis=1, name="Q_max")
+            # Important! not calculating gradient FROM this op to input node
+            self.endpoints["Q_target"] = tf.stop_gradient(q_target)
 
         with tf.variable_scope('loss'):
-            self.end_points["loss"] = tf.reduce_mean(tf.squared_difference(self.end_points["Q_target"],
-                                                                           self.end_points["Q_eval_wrt_a"],
-                                                                           name='TD_error'))
+            self.loss = tf.reduce_mean(tf.squared_difference(self.endpoints["Q_target"],
+                                                             self.endpoints["Q_eval_wrt_a"], name='TD_error'))
         with tf.variable_scope('train'):
-            optimizer = tf.train.MomentumOptimizer(learning_rate=self.learning_rate,
-                                                   momentum=0.9, name='Momentum',
-                                                   use_nesterov=False)
-            self.train_op = optimizer.minimize(self.end_points["loss"])
+            # self.train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+            self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
 
     def store_transition(self, s, a, r, s_next):
         # assert s,s_next -> np.array
@@ -150,20 +139,25 @@ class DeepQNetwork:
         self.memory.append(np.concatenate([s, [a], [r], s_next])) # deque
 
     def choose_action(self, state):
-        prob_choose = np.ones(self.n_actions) / self.n_actions * self.epsilon
         s = np.expand_dims(state, axis=0)
-        action_value = self.sess.run(self.end_points["Q_eval"], feed_dict={self.s: s})
-        # epsilon greedy
-        prob_choose[np.argmax(action_value)] = 1 - (self.n_actions - 1.0)/self.n_actions * self.epsilon
-        action = np.random.choice(self.action_space, p=prob_choose)
+        if np.random.uniform() < self.epsilon:
+            action = np.random.randint(0, self.n_actions)
+        else:
+            # forward feed the state and get q value for every actions
+            actions_value = self.sess.run(self.endpoints["Q_eval"], feed_dict={self.s: s})
+            action = np.argmax(actions_value)
         return action
 
     def learn(self):
+        memory_size = len(self.memory)
+        if memory_size < self.batch_size:
+            return
+
         # check to replace target parameters
-        if self.learn_step_counter % 300 == 0:
+        # if replace too frequent, the convergence will be not stable. sometime converge, sometime not
+        if self.learn_step_counter % 100 == 0:
             self.sess.run(self.target_replace_op)
 
-        memory_size = len(self.memory)
         sample_index = np.random.choice(range(memory_size), size=self.batch_size)  # replace = False
         batch_memory = np.array([self.memory[i] for i in sample_index])
 
@@ -177,123 +171,73 @@ class DeepQNetwork:
                      self.r: r.astype(np.float32),
                      self.s_next: s_next.astype(np.float32)}
 
-        _, cost = self.sess.run([self.train_op, self.end_points["loss"]], feed_dict=feed_dict)
-        self.cost_list.append(cost)
-        self.update_epsilon()
-        self.learn_step_counter += 1
-
-    def decay_epislon(self):
+        _, loss = self.sess.run([self.train_op, self.loss], feed_dict=feed_dict)
+        self.cost_log.append(loss)
         self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+        self.learn_step_counter += 1
 
     def plot_cost(self):
         import matplotlib.pyplot as plt
-        plt.plot(np.arange(len(self.cost_list)), self.cost_list)
+        plt.plot(np.arange(len(self.cost_log)), self.cost_log)
         plt.ylabel('Cost')
         plt.xlabel('training steps')
         plt.show()
 
 
-def interact(env, agent, num_episodes):
-    global_step = 0
-
-    for i_episode in range(num_episodes):
-        # reset env for each episode
-        state = env.reset()
-
-        while True:
-
-            action = agent.choose_action(state)
-            state_next, reward, done, info = env.step(action)
-            sars_sample = [state, action, reward, state_next]
-
-            agent.store(sars_sample)
-
-            if global_step > 200 and (global_step % 5 == 0):
-                agent.learn()
-
-            state = state_next
-            if done is True:
-                break
-
-            global_step += 1
-
-
 if __name__ == "__main__":
-    import gym
     from tqdm import tqdm
-
-    N_EPISODES = 100
+    import gym
 
     env = gym.make('CartPole-v0')
     # https://github.com/openai/gym/wiki/CartPole-v0
     # Solved Requirements
     # Considered solved when the average reward is greater than or equal to 195.0 over 100 consecutive trials.
-    env = env.unwrapped  # important ?
+
+    env = env.unwrapped
 
     print(env.action_space)
     print(env.observation_space)
-    # [position of cart, velocity of cart, angle of pole, rotation rate of pole]
     print(env.observation_space.high)
     print(env.observation_space.low)
 
     RL = DeepQNetwork(n_actions=env.action_space.n,
-                      n_features=env.observation_space.shape[0])
+                      n_features=env.observation_space.shape[0],
+                      learning_rate=0.01)
 
-    total_steps = 0
-
-    smooth = 5
-    smooth_score_log = []
-    score_log = deque(maxlen=smooth)
-
-    # pbar = tqdm(total=N_EPISODES)
-
+    N_EPISODES = 100
+    pbar = tqdm(total=N_EPISODES)
     for i_episode in range(N_EPISODES):
 
-        observation = env.reset()
-        ep_r = 0
+        state = env.reset()
         steps = 0
         while True:
-            steps += 1  # hold greater than 195 steps is a successful trial
-
+            steps += 1
             env.render()
 
-            action = RL.choose_action(observation)
+            action = RL.choose_action(state)
 
-            observation_, reward, done, info = env.step(action)
+            state_next, reward, done, info = env.step(action)
 
-            x, x_dot, theta, theta_dot = observation_  # 细分开, 为了修改原配的 reward
-            # x 是车的水平位移, 所以 r1 是车越偏离中心, 分越少
-            # theta 是棒子离垂直的角度, 角度越大, 越不垂直. 所以 r2 是棒越垂直, 分越高
-
+            # # the smaller theta and closer to center the better
+            # x, x_dot, theta, theta_dot = state_next
+            # # smaller reward, when not in the central
             # r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
+            # # smaller reward, when not perpendicular
             # r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            # custom_reward = r1 + r2  # 总 reward 是 r1 和 r2 的结合, 既考虑位置, 也考虑角度, 这样 DQN 学习更有效率
+            # reward = r1 + r2
 
-            RL.store_transition(observation, action, reward, observation_)
+            # if not use customized reward, the last reward +1 with done==True should be considered
+            # as an end game action, like a penalty.
+            reward = -1*reward if done is True else reward
 
-            # if total_steps > 1000:
-            #     RL.learn()
-
+            RL.store_transition(state, action, reward, state_next)
             RL.learn()
 
-            total_steps += 1
-            observation = observation_
-
             if done:
-                print('episode: ', i_episode,
-                      'steps: ', steps,
-                      'epsilon: ', round(RL.epsilon, 2))
-                score_log.append(steps)
-                smooth_score_log.append(np.mean(score_log))
                 break
+            state = state_next
 
-        # pbar.set_description('episode {}'.format(i_episode))
-        # pbar.set_postfix(episode_reward=round(np.mean(score_log), 2))
-        # pbar.update()
-
-    # RL.plot_cost()
-    import matplotlib.pyplot as plt
-    plt.plot(np.arange(len(smooth_score_log)), smooth_score_log)
-    plt.ylabel('avg episode reward')
-    plt.xlabel('training steps')
-    plt.show()
+        pbar.set_description('episode {}, epsilon {}'.format(i_episode, round(RL.epsilon, 2)))
+        pbar.set_postfix(steps=steps)
+        pbar.update()
+    pbar.close()
