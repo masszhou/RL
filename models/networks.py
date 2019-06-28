4016e842d3d4a6746463fecf7dbda5d813e9f561
Zhiliang Zhou <zhouzhiliang@gmail.com>
2019-06-28 11:02:57 +0200

    feat add BN after first fc layer

--- models/networks.py
+++ models/networks.py
@@ -16,15 +16,17 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
 
         init.xavier_uniform_(self.fc1.weight)
         init.xavier_uniform_(self.fc2.weight)
         init.uniform_(self.fc3.weight, -3e-3, 3e-3)
 
     def forward(self, state):
-        net = F.relu(self.fc1(state))
+        net = F.relu(self.bn1(self.fc1(state)))
         net = F.relu(self.fc2(net))
         net = torch.tanh(self.fc3(net))
         return net
@@ -40,6 +42,7 @@ class Critic(nn.Module):
         self.seed = torch.manual_seed(seed)
 
         self.fc1 = nn.Linear(state_size + action_size, fc1_units)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
 
@@ -49,7 +52,7 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         s_a = torch.cat([state, action], dim=1)
-        net = F.relu(self.fc1(s_a))
+        net = F.relu(self.bn1(self.fc1(s_a)))
         net = F.relu(self.fc2(net))
         net = self.fc3(net)  # no activation ?
         return net
