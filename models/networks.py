76a4b0f2e16973964cd70126cebd40a1453ec3ef
Zhiliang Zhou <zhouzhiliang@gmail.com>
2019-07-09 17:25:19 +0200

    update: more generic interface

--- models/networks.py
+++ models/networks.py
@@ -12,21 +12,35 @@ class Actor(nn.Module):
     input: state
     output: action
     """
-    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+    def __init__(self,
+                 in_shape,  # state_size
+                 out_shape,  # action_size
+                 seed=42,
+                 fc1_units=400,
+                 fc2_units=300):
+
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
+
+        self.fc1 = nn.Linear(in_shape, fc1_units)
         self.bn1 = nn.BatchNorm1d(fc1_units)
+
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
-        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.fc3 = nn.Linear(fc2_units, out_shape)
 
         init.xavier_uniform_(self.fc1.weight)
         init.xavier_uniform_(self.fc2.weight)
         init.uniform_(self.fc3.weight, -3e-3, 3e-3)
 
-    def forward(self, state):
-        net = F.relu(self.bn1(self.fc1(state)))
+    def forward(self, in_tensor):
+        """
+        deterministic policy
+        output action
+
+        :param in_tensor:
+        :return:
+        """
+        net = F.relu(self.bn1(self.fc1(in_tensor)))
         net = F.relu(self.fc2(net))
         net = torch.tanh(self.fc3(net))
         return net
@@ -37,22 +51,33 @@ class Critic(nn.Module):
     input: state, action
     output: Q_value
     """
-    def __init__(self, state_size, action_size, fc1_units=400, fc2_units=300, seed=2):
+    def __init__(self,
+                 in_shape,  # state_size + action_size
+                 out_shape=1,  # 1, action-value Q
+                 fc1_units=400,
+                 fc2_units=300,
+                 seed=2):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
 
-        self.fc1 = nn.Linear(state_size + action_size, fc1_units)
+        self.fc1 = nn.Linear(in_shape, fc1_units)
         self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
 
         init.xavier_uniform_(self.fc1.weight)
         init.xavier_uniform_(self.fc2.weight)
-        init.xavier_uniform_(self.fc3.weight)
+        init.uniform_(self.fc3.weight, -3e-3, 3e-3)
 
-    def forward(self, state, action):
-        s_a = torch.cat([state, action], dim=1)
-        net = F.relu(self.bn1(self.fc1(s_a)))
+    def forward(self, in_tensor):
+        """
+        output action-value Q
+        :param in_tensor: torch.cat((state, action), dim=1), rank=2, shape=[batch_size, state_size + action_size]
+        :return:
+        """
+        net = F.relu(self.bn1(self.fc1(in_tensor)))
         net = F.relu(self.fc2(net))
-        net = self.fc3(net)  # no activation ?
+        net = self.fc3(net)
+        # Q:no activation for last layer ?
+        # A:because action-value Q can be arbitrary number based on models, can not be normalized/scaled activation
         return net
